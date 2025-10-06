c782a35a9d439409743098808f7d462e33d4d17f
Ali Kawouk <kawouk652@gmail.com>
2025-10-06 23:34:59 +0300

    2nd commit

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -4,8 +4,31 @@ import { Component } from '@angular/core';
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css'
+  styleUrl: './dashboard.css',
 })
 export class Dashboard {
+  tickets = [
+    { id: 1, title: 'Network Issue', department: 'IT', status: 'Open', date: '2025-10-06' },
+    {
+      id: 2,
+      title: 'Printer not working',
+      department: 'Admin',
+      status: 'Assigned',
+      date: '2025-10-05',
+    },
+    { id: 3, title: 'Software update', department: 'IT', status: 'Resolved', date: '2025-10-03' },
+  ];
 
+  getStatusColor(status: string): string {
+    switch (status) {
+      case 'Open':
+        return 'primary';
+      case 'Assigned':
+        return 'warning';
+      case 'Resolved':
+        return 'success';
+      default:
+        return 'secondary';
+    }
+  }
 }
